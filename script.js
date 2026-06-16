644fa5bc73641408bd9c3aaf89205940dd44a316
Joseph101-ops <ufuomapraise15@gmail.com>
2026-06-16 01:51:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-// RESERVATION FORM
-document.getElementById("bookingForm").addEventListener("submit", function(e){
-e.preventDefault();
-alert("Reservation successful! We will contact you shortly.");
-this.reset();
-});
+// FORM SUBMIT HANDLED BY FORMSPREE (no JS needed)
 
 // CALL BUTTON
 function callNow(){
-window.location.href = "tel:+442034416996";
+window.location.href="tel:+442034416996";
 }
 
-// GOOGLE MAPS
+// MAP BUTTON
 function openMap(){
 window.open(
 "https://www.google.com/maps/search/?api=1&query=24+Great+Windmill+St+London+W1D+7LG",
 "_blank"
 );
 }
+
+
+// SCROLL ANIMATION (REVEAL SECTIONS)
+const sections = document.querySelectorAll(".section");
+
+const reveal = () => {
+sections.forEach(sec => {
+const top = sec.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 100){
+sec.classList.add("show");
+}
+});
+};
+
+window.addEventListener("scroll", reveal);
+reveal();
